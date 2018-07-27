fa25ea0152a194c7cb53e690ae80fcc4f2279996
lisakow <41714183+lisakow@users.noreply.github.com>
2018-07-26 19:22:49 -0500

    Update vx.js

--- vx.js
+++ vx.js
@@ -53,9 +53,8 @@ if (message.content.toLowerCase().startsWith(prefix + `ticket`)) {
     const reason = message.content.split(" ").slice(1).join(" ");
     if (!message.guild.roles.exists("name", "Support")) return message.channel.send(`This server doesn't have a \`Support\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
     if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
-     message.guild.createChannel('ticket').then(channel => channel.setParent(support));
-     var guild.channels.find('name', 'Ticket');
-     message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => { 
+    var message.guild.createChannel('ticket').then(channel => channel.setParent(support));
+    message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => { 
 
         let role = message.guild.roles.find("name", "Support");
         let role2 = message.guild.roles.find("name", "@everyone");
